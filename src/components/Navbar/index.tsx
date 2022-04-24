51e4d9952587e59740382f72d782259f04c4a54c
Luiz <doginuwu@gmail.com>
2022-04-24 01:38:26 -0300

    🐛 fix: change site link

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
                         </LinkNavigation>
                     ))}
                 </Content>
-                <a href="https://gotardo.vercel.app" target="_blank" rel="noreferrer">
+                <a href="https://gotardo.vercel.app" rel="noreferrer">
                     <Button color={theme.colors.white} textColor={theme.colors.black}>
                         Site principal
                     </Button>
