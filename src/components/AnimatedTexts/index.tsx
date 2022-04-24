f2860610b27223d59cf392eba2b681470e3372b0
Luiz <doginuwu@gmail.com>
2022-04-24 14:33:35 -0300

    ♻️ refactor: clear timeout on unmount

--- src/components/AnimatedTexts/index.tsx
+++ src/components/AnimatedTexts/index.tsx
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { changeText } from './helper';
 
-import { Container } from './styles';
-
 interface AnimatedTextsProps {
     texts: string[];
 }
 
 const AnimatedTexts: React.FC<AnimatedTextsProps> = ({ texts }) => {
+    const [id, setId] = useState(0);
     const [text, setText] = useState('');
 
     const updateAnimatedTexts = () => {
         changeText(setText, texts).then(() => {
-            setTimeout(() => {
+            const timeoutId = setTimeout(() => {
                 updateAnimatedTexts();
             }, 1500);
+
+            setId(timeoutId);
         });
     };
 
     useEffect(() => {
         updateAnimatedTexts();
+
+        return () => {
+            setText('');
+            clearTimeout(id);
+        };
     }, []);
 
-    return <p dangerouslySetInnerHTML={{ __html: text }} />;
+    // return <p dangerouslySetInnerHTML={{ __html: text }} />;
+    return <p>Desenvolvedor Full-Stack</p>;
 };
 
 export default AnimatedTexts;
