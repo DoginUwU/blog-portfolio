5f7e4db3b43bdcaaf966d5e1e0457c7c637a22d6
Luiz Eduardo <doginuwu@gmail.com>
2022-06-16 11:46:40 -0300

    .

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -25,11 +25,7 @@ const Navbar: React.FC = () => {
                         </LinkNavigation>
                     ))}
                 </Content>
-                <a href="https://gotardo.vercel.app" rel="noreferrer">
-                    <Button color={theme.colors.white} textColor={theme.colors.black}>
-                        Site principal
-                    </Button>
-                </a>
+                <div />
             </Container>
         </NavbarContainer>
     );
